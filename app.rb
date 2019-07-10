c45c59ac800c01a7664a86417f302e80cc87ca28
Jared Grove <jaredkgrove@gmail.com>
2019-07-10 14:40:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,8 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post "/puppies" do
+  post "/puppy" do
     @puppy = Puppy.new(params[:name], params[:breed], params[:months_old])
+    erb :display_puppy
   end
 end
